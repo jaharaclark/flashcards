0e4b6664dd70b82c528564ebc41017a9280951d6
Jahara Clark <jaharamclark@gmail.com>
2023-05-04 02:11:28 -0700

    Added incorrect test.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe Turn do
   before (:each) do
     @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @turn = Turn.new("Juneau", @card)
+    @card_b = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    @turn_b = Turn.new("Saturn", @card_b)
   end
 
   describe 'Initial Turn Creation' do
@@ -20,12 +22,21 @@ RSpec.describe Turn do
     end
   end
 
-  describe 'it can function' do
-    it 'can check a guess' do 
+  describe 'it can check guesses' do
+    it 'can indicate that a guess is correct' do
+      expect(@turn.guess).to eq("Juneau") 
       @turn.correct?
       expect(@turn.correct?).to be(true)
       @turn.feedback
       expect(@turn.feedback).to eq("Correct!")
+    end
+
+    it 'can indicate that a guess is incorrect' do
+      expect(@turn_b.guess).to eq("Saturn")
+      @turn_b.correct?
+      expect(@turn_b.correct?).to be(false)
+      @turn_b.feedback
+      expect(@turn_b.feedback).to eq("Incorrect.")
     end
   end
 end
